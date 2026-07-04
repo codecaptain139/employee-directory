22bbf5361d7136a0ecc176ba33459241dcde6abf
adarshchauhan139 <codecaptain139@gmail.com>
2026-07-04 15:36:41 +0530

    Add delete functionality using React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 // import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
-function EmployeeCard({name, role, country}) {
+function EmployeeCard({id, name, role, country, onDelete}) {
   return(
     <div className='card'>
       <p>{name}</p>
       <p>{role}</p>
       <p>{country}</p>
+      <button onClick={() => onDelete(id)}>
+        Delete
+      </button>
     </div>
   )
 }
 
 function App() {
-  const employeeData = [
+  const [employees, setEmployees] = useState([
     {
       id: 1,
       name: "Zarvis",
@@ -23,27 +27,39 @@ function App() {
       id: 2,
       name: "Lucifer",
       role: "Backend Developer",
-      country: "India"
+      country: "USA"
     },
     {
       id: 3,
       name: "Omnitrex",
       role: "Fullstack Developer",
-      country: "India"
+      country: "Japan"
     }
   ]
+);
+
+const handleDelete = (id) => {
+  const updateEmployees = employees.filter((employee) => {
+    return employee.id !== id;
+  });
+
+
+  setEmployees(updateEmployees);
+}
 
   return(
     <div className='app'>
       <h1>Employee Directory</h1>
 
       {
-        employeeData.map((employee) => (
+        employees.map((employee) => (
           <EmployeeCard
-            key={employee.id}
-            name={employee.name}
-            role={employee.role}
-            country={employee.country}
+            key = {employee.id}
+            id = {employee.id}
+            name = {employee.name}
+            role = {employee.role}
+            country = {employee.country}
+            onDelete = {handleDelete}
           />
         ))
       }
@@ -51,4 +67,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
