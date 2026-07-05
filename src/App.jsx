c0ad63279b34782cd332afbe24e64d6ddfb1c1ad
adarshchauhan139 <codecaptain139@gmail.com>
2026-07-05 15:25:06 +0530

    Add employee functionality using react state

--- src/App.jsx
+++ src/App.jsx
@@ -41,15 +41,51 @@ function App() {
 const handleDelete = (id) => {
   const updateEmployees = employees.filter((employee) => {
     return employee.id !== id;
-  });
-
+});
 
   setEmployees(updateEmployees);
 }
 
+const handleAddEmployee = () => {
+  const newEmployee = {
+    id: employees.length + 1,
+    name,
+    role,
+    country
+  }
+
+  setEmployees([...employees, newEmployee])
+  setName("");
+  setRole("");
+  setCountry("");
+
+  console.log(newEmployee);
+}
+
+const [name, setName] = useState("");
+const [role, setRole] = useState("");
+const [country, setCountry] = useState("");
+
   return(
     <div className='app'>
       <h1>Employee Directory</h1>
+
+      <div className="form-group">
+        <label>Name: </label>
+        <input type="text" placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)}/>
+      </div>
+
+      <div className="form-group">
+        <label>Role: </label>
+        <input type="text" placeholder='Enter Role' value={role} onChange={(e) => setRole(e.target.value)}/>
+      </div>
+
+      <div className="form-group">
+        <label>Country: </label>
+        <input type="text" placeholder='Enter Country' value={country} onChange={(e) => setCountry(e.target.value)}/>
+      </div>
+
+      <button onClick={handleAddEmployee}>Add Employee</button>
 
       {
         employees.map((employee) => (
